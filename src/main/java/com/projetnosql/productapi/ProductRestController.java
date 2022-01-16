13e019fb30c7afd6871c2c456f4c88356acdb713
rawaa <rawaatrad@yahoo.com>
2022-01-16 11:13:33 +0100

    first commit

---
+++ src/main/java/com/projetnosql/productapi/ProductRestController.java
@@ -0,0 +1,21 @@
+package com.projetnosql.productapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductRestController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+}
+
+
